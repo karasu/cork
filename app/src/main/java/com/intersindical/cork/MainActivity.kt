7479cf65697c01106a007aac9da4da80bf058d76
karasu <gustaucastells@gmail.com>
2021-06-21 22:49:14 +0200

    Auth is working.

--- app/src/main/java/com/intersindical/cork/MainActivity.kt
+++ app/src/main/java/com/intersindical/cork/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.intersindical.cork
 
-//import com.google.android.gms.common.GoogleApiAvailability
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -14,17 +14,27 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.android.gms.location.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 //import com.firebase.ui.auth.AuthUI
 //import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 //import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 //import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
@@ -33,9 +43,24 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.math.*
 
-data class Nis (
-    var ni : String,
-    var nom : String)
+data class User (
+    var email : String,
+    var password : String,
+    var name : String
+)
+
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Activity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,83 +73,115 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var centre : Centre
 
-    // private lateinit var database: DatabaseReference
     private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
 
-    private lateinit var currentNIF: String
-
-    private var nis : List<Nis> = listOf(
-        Nis("46693925", "GONZALO ALCARAZ RUIZ"),
-        Nis("39905310", "ANNA ALEGRE FERNANDEZ"),
-        Nis("46667185", "CARLES AMIGÓ RODRIGO"),
-        Nis("77728278", "ROSA BRUCART PRAT"),
-        Nis("52460266", "MIQUEL ÀNGEL CAMACHO VEGA"),
-        Nis("77909824", "MONTSE CARRERAS BARNÉS"),
-        Nis("39332661", "TONI CASSERRAS GASOL"),
-        Nis("43559189", "ROGER CASTELLANOS CORBERA"),
-        Nis("44990832", "GUSTAU CASTELLS APARICIO"),
-        Nis("77612612", "JORDI CLOPES GARRELL"),
-        Nis("46139555", "JORDI ESPOT BENAZET"),
-        Nis("40452328", "GERARD FUREST DALMAU"),
-        Nis("52595463", "LLUÍS HURTADO NOGUER"),
-        Nis("43735629", "MERCÈ IBERN NOVELL"),
-        Nis("46631108", "JOSEP ANTON MARCÉ BUSQUETS"),
-        Nis("46588790", "ESTER MARLES XAUS"),
-        Nis("47933418", "ELI MARTÍNEZ SALAMÓ"),
-        Nis("38144090", "MARTA MOLINA ÀLVAREZ"),
-        Nis("40312977", "NÚRIA PASTOR GAMERO"),
-        Nis("40346988", "BERNAT PELACH SAGET"),
-        Nis("53070896", "LAURA PÉREZ PÉREZ"),
-        Nis("43698734", "ALBA PEREZ XAUS"),
-        Nis("33956254", "Eloi Planas Vila"),
-        Nis("52605521", "JOAN ANTONI PONS ALBALAT"),
-        Nis("47675111", "Judith Ribera Salvia"),
-        Nis("38111152", "ANGEL MARIA SAGARRA GUITART"),
-        Nis("47714590", "MARC SANTASUSANA CORZAN"),
-        Nis("38866187", "LUARD SILVESTRE CASTELLÓ"),
-        Nis("46629777", "Aina Solà Rodrigo")
+    private var users : List<User> = listOf(
+        User("gonzalo.alcaraz@intersindical-csc.cat","46693925", "GONZALO ALCARAZ RUIZ"),
+        User("anna.alegre@intersindical-csc.cat","39905310", "ANNA ALEGRE FERNANDEZ"),
+        User("carles.amigo@intersindical-csc.cat","46667185", "CARLES AMIGÓ RODRIGO"),
+        User("rosa.brucart@intersindical-csc.cat","77728278", "ROSA BRUCART PRAT"),
+        User("miquelangel.camacho@intersindical-csc.cat","52460266", "MIQUEL ÀNGEL CAMACHO VEGA"),
+        User("montse.carreras@intersindical-csc.cat","77909824", "MONTSE CARRERAS BARNÉS"),
+        User("toni.casserras@intersindical-csc.cat","39332661", "TONI CASSERRAS GASOL"),
+        User("roger.castellanos@intersindical-csc.cat","43559189", "ROGER CASTELLANOS CORBERA"),
+        User("gustau.castells@intersindical-csc.cat","44990832", "GUSTAU CASTELLS APARICIO"),
+        User("jordi.clopes@intersindical-csc.cat","77612612", "JORDI CLOPES GARRELL"),
+        User("jordi.espot@intersindical-csc.cat","46139555", "JORDI ESPOT BENAZET"),
+        User("gerard.furest@intersindical-csc.cat","40452328", "GERARD FUREST DALMAU"),
+        User("lluis.hurtado@intersindical-csc.cat","52595463", "LLUÍS HURTADO NOGUER"),
+        User("merce.ibern@intersindical-csc.cat","43735629", "MERCÈ IBERN NOVELL"),
+        User("josep.marce@intersindical-csc.cat","46631108", "JOSEP ANTON MARCÉ BUSQUETS"),
+        User("ester.marles@intersindical-csc.cat","46588790", "ESTER MARLES XAUS"),
+        User("elisabeth.martinez@intersindical-csc.cat","47933418", "ELI MARTÍNEZ SALAMÓ"),
+        User("marta.molina@intersindical-csc.cat","38144090", "MARTA MOLINA ÀLVAREZ"),
+        User("nuria.pastor@intersindical-csc.cat","40312977", "NÚRIA PASTOR GAMERO"),
+        User("bernat.pelach@intersindical-csc.cat","40346988", "BERNAT PELACH SAGET"),
+        User("laura.perez@intersindical-csc.cat","53070896", "LAURA PÉREZ PÉREZ"),
+        User("alba.perez@intersindical-csc.cat","43698734", "ALBA PEREZ XAUS"),
+        User("eloi.planas@intersindical-csc.cat","33956254", "Eloi Planas Vila"),
+        User("toni.pons@intersindical-csc.cat","52605521", "JOAN ANTONI PONS ALBALAT"),
+        User("judith.ribera@intersindical-csc.cat","47675111", "Judith Ribera Salvia"),
+        User("angel.sagarra@intersindical-csc.cat","38111152", "ANGEL MARIA SAGARRA GUITART"),
+        User("marc.santasusana@intersindical-csc.cat","47714590", "MARC SANTASUSANA CORZAN"),
+        User("luard.silvestre@intersindical-csc.cat","38866187", "LUARD SILVESTRE CASTELLÓ"),
+        User("aina.sola@intersindical-csc.cat","46629777", "Aina Solà Rodrigo")
     )
 
-    /*
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
-    }
-
-    private fun createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build())
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_create_intent]
-    }
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+    private fun createUsers() {
+        for (item in users) {
+            auth.createUserWithEmailAndPassword(item.email, item.password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        //Log.d(TAG, "createUserWithEmail:success")
+                        // val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        //Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
         }
     }
 
-     */
+    // Authentication
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload();
+        }
+    }
+    private fun reload() {
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    //Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    //Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "L'autenticació ha fallat.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun updateUI(user : FirebaseUser?) {
+        if (user != null) {
+            hideKeyboard()
+
+            binding.loginButton.visibility = View.INVISIBLE
+            binding.identificat.visibility = View.INVISIBLE
+            binding.email.visibility = View.INVISIBLE
+            binding.password.visibility = View.INVISIBLE
+
+            binding.school.visibility = View.VISIBLE
+            binding.visitButton.visibility = View.VISIBLE
+            binding.searchSchoolButton.visibility = View.VISIBLE
+
+            // loadCentresButton.visibility = View.VISIBLE
+            getLastLocation()
+
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -132,26 +189,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         //setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         binding.school.visibility = View.INVISIBLE
         binding.visitButton.visibility = View.INVISIBLE
         binding.loadCentresButton.visibility = View.INVISIBLE
         binding.searchSchoolButton.visibility = View.INVISIBLE
 
-        //identificat.visibility = View.INVISIBLE
-        //nif.visibility = View.INVISIBLE
-        //loginButton.visibility = View.INVISIBLE
+
+
+
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            // User is signed in
+            // updateUI(user)
+            Firebase.auth.signOut()
+        } else {
+            // No user is signed in
+        }
 
         loadCentres()
+
+        // Force the creation of all users in database
+        // createUsers()
 
         if (!isOnline()) {
             Toast.makeText(this@MainActivity, "Sense connexió!", Toast.LENGTH_LONG)
                 .show()
         }
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding.loadCentresButton.setOnClickListener {
             loadCentres()
@@ -167,24 +237,11 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
             else {
-                if (checkLogin(binding.nif.text.toString())) {
-                    // Login ok
-                    currentNIF = binding.nif.text.toString()
-                    binding.loginButton.visibility = View.INVISIBLE
-                    binding.identificat.visibility = View.INVISIBLE
-                    binding.nif.visibility = View.INVISIBLE
+                val myEmail : String = binding.email.text.toString()
+                val myPassword : String = binding.password.text.toString()
 
-                    binding.school.visibility = View.VISIBLE
-                    binding.visitButton.visibility = View.VISIBLE
-                    binding.searchSchoolButton.visibility = View.VISIBLE
+                signIn(myEmail, myPassword)
 
-                    // loadCentresButton.visibility = View.VISIBLE
-                    getLastLocation()
-
-                } else {
-                    Toast.makeText(this@MainActivity, R.string.invalidlogin, Toast.LENGTH_SHORT)
-                        .show()
-                }
             }
         }
 
@@ -193,8 +250,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.visitButton.setOnClickListener {
             val currentTime = java.util.Calendar.getInstance()
-            centre.visitTime = currentTime.toString()
-            centre.currentNIF = currentNIF
+            centre.visitTime = currentTime.time.toString()
+            centre.user = auth.currentUser!!.email.toString()
             database.getReference("centres").child(centre.Codi!!).setValue(centre)
 
             Toast.makeText(
@@ -204,6 +261,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 
     // ---------------------------------------------------------------------------------------------
 
@@ -359,11 +417,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkLogin(loginid: String) : Boolean {
-        for (item in nis) {
-            if (item.ni == loginid) {
+        for (item in users) {
+            if (item.password == loginid) {
                 Toast.makeText(
                     this@MainActivity,
-                    item.nom,
+                    item.name,
                     Toast.LENGTH_SHORT
                 )
                     .show()
@@ -421,7 +479,7 @@ data class Centre(
     @SerializedName("E-mail_centre")
     var email : String?=null,
     var visitTime : String,
-    var currentNIF : String)
+    var user : String)
 
 
 /*
